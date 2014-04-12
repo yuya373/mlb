19f22093cb123754f775bc79bcac11ba1e45e157
yuya373 <yuya373@me.com>
2014-04-12 22:24:07 +0900

    add model batter

---
+++ app/models/batter.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: batters
+#
+#  id         :integer          not null
+#  first_name :string(255)      not null
+#  last_name  :string(255)      not null
+#  bats       :integer          not null
+#  pos        :integer          not null
+#  number     :integer          not null
+#  team_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_batters_on_id       (id) UNIQUE
+#  index_batters_on_team_id  (team_id)
+#
+
+class Batter < ActiveRecord::Base
+  include Scrapable
+
+  class << self
+    def fetch
+      ::Team::TEAM_ID.each do |team_id|
+        uri = BASE_URL +
+          "components/team/stats/#{team_id}-stats.xml"
+        batter = Nokogiri::XML.parse(open(uri)).css('batter')
+      end
+    end
+  end
+end
---
+++ db/migrate/20140412102304_create_batters.rb
@@ -0,0 +1,17 @@
+class CreateBatters < ActiveRecord::Migration
+  def change
+    create_table :batters, id: false do |t|
+      t.integer :id, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.integer :bats, limit: 1, null: false
+      t.integer :pos, limit: 1, null: false
+      t.integer :number, null: false
+      t.belongs_to :team, null: false
+      t.timestamps
+    end
+
+    add_index :batters, :team_id
+    add_index :batters, :id, unique: true
+  end
+end
