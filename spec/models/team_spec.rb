2f599ab652c1741e607c6a50e4e5bd1cddab125b
yuya373 <yuya373@me.com>
2014-04-12 22:19:18 +0900

    annotate team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,13 +2,16 @@
 #
 # Table name: teams
 #
-#  id         :integer          not null, primary key
-#  team_id    :integer          not null
+#  id         :integer          not null
 #  league_id  :integer          not null
 #  name       :string(255)      not null
 #  abbrev     :string(255)      not null
 #  created_at :datetime
 #  updated_at :datetime
+#
+# Indexes
+#
+#  index_teams_on_id  (id) UNIQUE
 #
 
 require 'spec_helper'
