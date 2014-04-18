64ec4e9817caaccf2905cd997744e278ca54f97b
yuya373 <yuya373@me.com>
2014-04-18 21:38:39 +0900

    fix

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,8 +1,8 @@
 app_path = '/home/vagrant/mlb'
-worker_processes 4
+worker_processes 1
 working_directory "#{app_path}/current"
 
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "#{app_path}/current/tmp/sockets/unicorn.sock"
 timeout 20
 
 pid "#{app_path}/current/tmp/pids/unicorn.pid"
