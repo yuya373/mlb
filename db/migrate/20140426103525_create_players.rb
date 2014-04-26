d5bbe1dab8e24f365326af570461b9d0e8b68009
yuya373 <yuya373@me.com>
2014-04-27 00:18:02 +0900

    add model player

---
+++ db/migrate/20140426103525_create_players.rb
@@ -0,0 +1,18 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players , id: false do |t|
+      t.integer :id, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.integer :bats, limit: 1, null: false
+      t.integer :throws, limit: 1, null: false
+      t.integer :pos, limit: 1, null: false
+      t.integer :jersey_number, null: false
+      t.belongs_to :team, null: false
+      t.timestamps
+    end
+
+    add_index :players, :team_id
+    add_index :players, :id, unique: true
+  end
+end
