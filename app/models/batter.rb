b0ccb51b4ed830c0ce21ee4fb8ac0a8a9d636c50
yuya373 <yuya373@me.com>
2014-04-27 00:19:12 +0900

    batter belongs to player

--- app/models/batter.rb
+++ app/models/batter.rb
@@ -20,43 +20,15 @@
 #
 
 class Batter < ActiveRecord::Base
- self.primary_key = 'id'
-  belongs_to :team
-  has_one :batter_stat, foreign_key: :batter_id
-
   include Scrapable
-
-  BATS = {
-    'R' => 0,
-    'L' => 1,
-    'S' => 2
-  }.freeze
-
-  THROWS = {
-    'R' => 0,
-    'L' => 1
-  }.freeze
-
-  POS = {
-    'D'  => 0,
-    'P' => 1,
-    'C'  => 2,
-    '1B' => 3,
-    '2B' => 4,
-    '3B' => 5,
-    'SS' => 6,
-    'LF' => 7,
-    'CF' => 8,
-    'RF' => 9,
-    'O'  => 10
-  }.freeze
+  belongs_to :player
 
   class << self
     def create_or_update(batter)
       batter.each do |b|
         attr = normarize(b.attributes)
-        batter = find_or_initialize_by(id: attr[:id])
-        batter.update!(attr)
+        stats = find_or_initialize_by(player_id: attr[:player_id])
+        stats.update!(attr)
       end
     end
 
@@ -67,31 +39,11 @@ class Batter < ActiveRecord::Base
       attr.each do |_, v|
         key = v.name.downcase
         v = v.value
-        key, v = name_to_first_name(key, v) if key == 'name_display_first_last'
-        v = convert_value(key, v)
-        normarized[key.to_sym] = v if Batter.attribute_names.include? key
+        key = 'player_' + key if key == "id"
+        normarized[key.to_sym] = v.to_i if Batter.
+          attribute_names.include? key
       end
       normarized
-    end
-
-    def name_to_first_name(k, v)
-      v = v.gsub!(/\s.*/, '')
-      ['first_name', v]
-    end
-
-    def convert_value(k, v)
-      case k
-      when 'bats'
-        return BATS[v]
-      when 'pos'
-        return POS[v]
-      when 'throws'
-        return THROWS[v]
-      when 'id'
-        return v.to_i
-      else
-        return v
-      end
     end
   end
 end
