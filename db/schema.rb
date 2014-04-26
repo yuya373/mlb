72b3f01884b2136a492ee3fec875d30ad64d1337
yuya373 <yuya373@me.com>
2014-04-27 00:20:20 +0900

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412155057) do
+ActiveRecord::Schema.define(version: 20140426103525) do
 
   create_table "batter_stats", force: true do |t|
     t.integer  "batter_id",  null: false
@@ -42,7 +42,36 @@ ActiveRecord::Schema.define(version: 20140412155057) do
 
   add_index "batter_stats", ["batter_id"], name: "index_batter_stats_on_batter_id", unique: true, using: :btree
 
-  create_table "batters", id: false, force: true do |t|
+  create_table "batters", force: true do |t|
+    t.integer  "player_id",  null: false
+    t.integer  "h",          null: false
+    t.integer  "ab",         null: false
+    t.integer  "tb",         null: false
+    t.integer  "r",          null: false
+    t.integer  "b2",         null: false
+    t.integer  "b3",         null: false
+    t.integer  "hr",         null: false
+    t.integer  "rbi",        null: false
+    t.integer  "sac",        null: false
+    t.integer  "sf",         null: false
+    t.integer  "hbp",        null: false
+    t.integer  "bb",         null: false
+    t.integer  "ibb",        null: false
+    t.integer  "so",         null: false
+    t.integer  "sb",         null: false
+    t.integer  "cs",         null: false
+    t.integer  "gidp",       null: false
+    t.integer  "np",         null: false
+    t.integer  "go",         null: false
+    t.integer  "ao",         null: false
+    t.integer  "tpa",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "batters", ["player_id"], name: "index_batters_on_player_id", unique: true, using: :btree
+
+  create_table "players", id: false, force: true do |t|
     t.integer  "id",                      null: false
     t.string   "first_name",              null: false
     t.string   "last_name",               null: false
@@ -55,8 +84,8 @@ ActiveRecord::Schema.define(version: 20140412155057) do
     t.datetime "updated_at"
   end
 
-  add_index "batters", ["id"], name: "index_batters_on_id", unique: true, using: :btree
-  add_index "batters", ["team_id"], name: "index_batters_on_team_id", using: :btree
+  add_index "players", ["id"], name: "index_players_on_id", unique: true, using: :btree
+  add_index "players", ["team_id"], name: "index_players_on_team_id", using: :btree
 
   create_table "teams", id: false, force: true do |t|
     t.integer  "id",         null: false
